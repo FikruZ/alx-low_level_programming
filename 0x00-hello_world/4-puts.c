2394dc54d3c3e97825a79f5c4c175f0d388b8bf9
FikruZ <fikruzmulat@gmail.com>
2022-07-02 10:30:26 -0700

    task 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+/**
+ * Description: Main - Entry point
+ * * *
+ * Return: Always 0 (Success
+ * **/)
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
